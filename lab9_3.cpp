3018f83b6b3384a67449636220b3904548d92c8e
PEEMTM <p2706@hotmail.com>
2021-01-08 16:13:55 +0700

    finish

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+    int count=0;
+    float sum,sum2=0,mean,mean2,sd,x=0;
+    string textline;
+    ifstream source;
+    source.open("score.txt");
+    while(getline(source,textline)){
+        x=stof(textline);
+        sum+=x;
+        sum2+=pow(x,2);
+        count++;
+    }
+    mean=sum/count;
+    mean2=pow(mean,2);
+    sd=sqrt((sum2/count)-mean2);
+    cout << "Number of data = "<<count<<endl;
+    cout << setprecision(3);
+    cout << "Mean = "<<mean<<endl;
+    cout << "Standard deviation = "<<sd;
+    source.close();
+    return 0;
+}
